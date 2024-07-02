988f51c8c12f28696c09822572d1509871f68709
shkolykivan <shkolikivan@gmail.com>
2024-07-02 20:29:47 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,61 @@
+const menuButton = document.querySelector('.full-screen__show-menu-button');
+
+menuButton.addEventListener("click", () => {
+    if (menuButton.classList.contains('active')) {
+        menuButton.classList.remove('active')
+        menuButton.firstChild.textContent = 'Show menu'
+        document.querySelector('.full-screen__list.list').style.display = "none"
+        document.querySelector('.header').style.zIndex = '2'
+    } else {
+        menuButton.classList.add('active')
+        menuButton.firstChild.textContent = 'Hide menu'
+        document.querySelector('.full-screen__list.list').style.display = "block"
+        document.querySelector('.header').style.zIndex = '0'
+    }
+})
+
+document.querySelectorAll(".list__link").forEach(a => {
+    a.addEventListener('click', () => {
+        if (menuButton.classList.contains('active')) {
+            menuButton.classList.remove('active')
+            menuButton.firstChild.textContent = 'Show menu'
+            document.querySelector('.full-screen__list.list').style.display = "none"
+            document.querySelector('.header').style.zIndex = '2'
+        }
+    })
+})
+
+
+
+window.addEventListener('click', (event) => {
+    const target = event.target
+    if (target.classList.contains('footer__hidden-menu')) {
+        if (target.classList.contains('active')) {
+            target.classList.remove('active')
+            target.firstChild.textContent = 'Show menu'
+            document.querySelector('.footer__list').style.maxHeight = '40px'
+        } else {
+            target.classList.add('active')
+            target.firstChild.textContent = 'Hide menu'
+            document.querySelector('.footer__list').style.maxHeight = 'none'
+        }
+    }
+})
+window.addEventListener('click', (event) => {
+    const target = event.target
+    if (target.classList.contains('blogs__hidden-menu')) {
+        if (target.classList.contains('active')) {
+            target.classList.remove('active')
+            target.firstChild.textContent = 'Show menu'
+            document.querySelector('.blogs__list').style.maxHeight = '40px'
+        } else {
+            target.classList.add('active')
+            target.firstChild.textContent = 'Hide menu'
+            document.querySelector('.blogs__list').style.maxHeight = 'none'
+        }
+    }
+})
+
+
+console.log(innerWidth)
+console.log(innerHeight)
